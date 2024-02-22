c9b29d9551ea32106baa8a0df553e33181b68d1a
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2024-02-23 00:57:38 +0100

    made newsletter modal responsive

--- src/components/footer/newsletterModal.tsx
+++ src/components/footer/newsletterModal.tsx
@@ -16,7 +16,7 @@ export const NewsletterModal: React.FC<NewsletterModalProps> = ({
       onClick={handleClick}
       className="top-0 left-0 fixed h-screen w-screen  bg-[rgba(0,0,0,.3)] z-50 grid justify-center items-center montserrat-regular "
     >
-      <div className="  w-96 h-64 bg-white grid justify-center px-10 border border-gray-600 rounded-sm">
+      <div className="  w-96 h-64 bg-white grid justify-center px-10 border border-gray-600 rounded-sm max-[410px]:w-72">
         <h1 className=" text-center my-10 text-2xl font-bold">
           Thank you for signing up to our newsletter!
         </h1>
